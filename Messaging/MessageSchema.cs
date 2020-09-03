6aca2dde05d73e2f3eeb8dd04e0089bb4028db0f
billchetch <45006134+billchetch@users.noreply.github.com>
2020-09-03 13:36:34 +0800

    s

---
+++ Messaging/MessageSchema.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chetch.Messaging
+{
+    /// <summary>
+    /// Use this class to define message values (both populating and retreiving)...
+    /// The idea is to derive a domain specific schema from this class to use in populating and retreiving data in a consistent and structured form.
+    /// </summary>
+    public class MessageSchema
+    {
+        public Message Message { get; set; }
+
+        public MessageSchema() { }
+    }
+}
