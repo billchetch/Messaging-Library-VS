4011569922e83b864b67ca35dd60196e91085b87
billchetch <45006134+billchetch@users.noreply.github.com>
2020-12-09 22:59:49 +0800

    s

--- Messaging/MessageSchema.cs
+++ Messaging/MessageSchema.cs
@@ -12,6 +12,16 @@ namespace Chetch.Messaging
     /// </summary>
     public class MessageSchema
     {
+        static public T CreateSchema<T>(MessageType messageType, String target) where T : MessageSchema, new()
+        {
+            Message msg = new Message(messageType);
+            msg.Target = target;
+            T t = new T();
+            t.Message = msg;
+
+            return t;
+        }
+
         public Message Message { get; set; }
 
 
